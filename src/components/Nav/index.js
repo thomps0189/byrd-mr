c4524a0400e30dca60ae45d367c414cc45eafd53
thomps0189 <angelathompson2011@gmail.com>
2022-03-21 11:30:26 -0500

    Navigation bar with initial CSS styling. Added Logo to top of homepage

---
+++ src/components/Nav/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+const HeaderLogo = require("../../assets/Images/Logo.png");
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="#store">Store</a>
+        </li>
+        <li>
+          <a href="#tutorials">Tutorials</a>
+        </li>
+        <div>
+          <img id="header-img" src={HeaderLogo} alt="logo" />
+        </div>
+        {/* <HeaderLogo /> */}
+        <li>
+          <a href="#blog">Blog</a>
+        </li>
+        <li>
+          <a href="#Contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
