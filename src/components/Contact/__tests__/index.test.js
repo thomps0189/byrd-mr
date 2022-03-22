ba6d3e76451132087b158125ef6a96739362b7e0
thomps0189 <angelathompson2011@gmail.com>
2022-03-22 15:17:49 -0500

    contact page rendering. test passed

---
+++ src/components/Contact/__tests__/index.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Contact from "..";
+
+afterEach(cleanup);
+
+describe("Contact component", () => {
+  it("renders", () => {
+    render(<Contact />);
+  });
+
+  it("renders", () => {
+    const { asFragment } = render(<Contact />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
